de56283af7c29fc346d51ed775327569aeec5ae4
Dell <jumabekovradik@gmail.com>
2022-04-26 18:31:42 +0600

    DenDev

---
+++ src/main/java/com/itacademy/hakaton/dendev/DendevApplication.java
@@ -0,0 +1,13 @@
+package com.itacademy.hakaton.dendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DendevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DendevApplication.class, args);
+    }
+
+}
